b018bff096fcf7ba532d14a4937e388ca1e71f03
guo chu <guochu604b@gmail.com>
2024-02-12 07:41:47 +0800

    gc cahnges

--- examples/fourthpaper/oneorbital/plot_fig1b.py
+++ examples/fourthpaper/oneorbital/plot_fig1b.py
@@ -64,14 +64,14 @@ def read_tempo_data(Vs, U, dt=0.014, order=7):
 	return rescale(Ilefts), rescale(Irights), bds
 
 
-def read_tempo_data2(Vs, U, dt, order=7, k=5, chi=160):
+def read_tempo_data2(Vs, U, dt, order=7, k=5, chi=160, prony=5):
 	t = 6.3
 	N = round(t / dt)
 	Ilefts = []
 	Irights = []
 	bds = []
 	for V in Vs:
-		filename = 'result/ti_V%s_U%s_N%s_dt%s_order%s_chi%s_k%s.json'%(V, U, N, dt, order, chi, k)
+		filename = 'result/ti_V%s_U%s_N%s_dt%s_order%s_chi%s_k%s_prony%s.json'%(V, U, N, dt, order, chi, k, prony)
 		ts, Ileft, Iright, bd = read_tempo_data_single(filename)
 		Ilefts.append(rescale(Ileft))
 		Irights.append(rescale(Iright))
@@ -79,14 +79,14 @@ def read_tempo_data2(Vs, U, dt, order=7, k=5, chi=160):
 	return Ilefts, Irights, bds
 
 
-Vs = [0.17857143, 0.35714286, 0.53571429, 0.71428571, 0.89285715]
+Vs = [0.17857143, 0.35714286, 0.53571429, 0.71428571, 0.89285715, 1.07142857]
 colors = ['b', 'g', 'c', 'y', 'r']
 markers = ['o', '^', '+', 'x', '*']
 
 ed_Ilefts_old, ed_Irights_old = read_ed_data(Vs)
 ed_Imean_old = 0.5 * ( ed_Ilefts_old - ed_Irights_old )
 
-ed_Ilefts, ed_Irights = read_ed_data_n(Vs)
+ed_Ilefts, ed_Irights = read_ed_data_n(Vs, dw=0.0005)
 ed_Imean = 0.5 * ( ed_Ilefts - ed_Irights )
 # print(ed_Ilefts)
 # print(ed_Irights)
@@ -94,6 +94,10 @@ ed_Imean = 0.5 * ( ed_Ilefts - ed_Irights )
 
 print(ed_Imean_old)
 print(ed_Imean)
+
+# ed_Ilefts_tmp, ed_Irights_tmp = read_ed_data_n(Vs, 0.0005)
+# ed_Imean_tmp = 0.5 * ( ed_Ilefts_tmp - ed_Irights_tmp )
+# print(ed_Imean_tmp)
 
 dt = 0.014
 ts = linspace(4.2, 6.3, num=16)
@@ -108,6 +112,12 @@ Imean_0_old = 0.5 * ( Ilefts_0_old - Irights_0_old )
 
 Ilefts_0, Irights_0, bd_0 = read_tempo_data2(Vs, U, dt=dt, order=order, k=k, chi=chi)
 Imean_0 = [0.5 * ( a - b ) for a, b in zip(Ilefts_0, Irights_0)]
+
+# print(Ilefts_0_old)
+# print(Irights_0_old)
+
+# print(Ilefts_0[4])
+# print(Irights_0[4])
 
 # print(Imean_0[4])
 
@@ -135,31 +145,45 @@ Imean_4 = [0.5 * ( a - b ) for a, b in zip(Ilefts_4, Irights_4)]
 # print(Imean_4_old[0])
 # print(Imean_4[0])
 
-# print([item[0] for item in Imean_4])
-# print([item[-1] for item in Imean_4])
+U = 6.
+Ilefts_6_old, Irights_6_old, bd_6_old = read_tempo_data(Vs, U)
+Imean_6_old = 0.5 * ( Ilefts_6_old - Irights_6_old )
 
-# U = 6.
-# Ilefts_6, Irights_6, bd_6 = read_tempo_data2(Vs, U, dt=dt, order=order, k=k, chi=chi)
-# Imean_6 = [0.5 * ( a - b ) for a, b in zip(Ilefts_6, Irights_6)]
+Ilefts_6, Irights_6, bd_6 = read_tempo_data2(Vs, U, dt=dt, order=order, k=k, chi=chi)
+Imean_6 = [0.5 * ( a - b ) for a, b in zip(Ilefts_6, Irights_6)]
 
 # U = 8.
 # Ilefts_8, Irights_8, bd_8 = read_tempo_data(Vs2, U)
 # Imean_8 = 0.5 * ( Ilefts_8 - Irights_8 )
 
 
-# print(Ilefts_8)
-# print(Irights_8)
-# print(asarray(Ilefts_8) + asarray(Irights_8))
+Imeans_allU_0 = [Imean_0[0], Imean_2[0], Imean_4[0], Imean_6[0]]
+Imeans_allU_1 = [Imean_0[1], Imean_2[1], Imean_4[1], Imean_6[1]]
+Imeans_allU_2 = [Imean_0[2], Imean_2[2], Imean_4[2], Imean_6[2]]
+Imeans_allU_3 = [Imean_0[3], Imean_2[3], Imean_4[3], Imean_6[3]]
+Imeans_allU_4 = [Imean_0[4], Imean_2[4], Imean_4[4], Imean_6[4]]
+Imeans_allU_5 = [Imean_0[5], Imean_2[5], Imean_4[5], Imean_6[5]]
 
-Imeans_allU_0 = [Imean_0[0], Imean_2[0], Imean_4[0]]
-Imeans_allU_1 = [Imean_0[1], Imean_2[1], Imean_4[1]]
-Imeans_allU_2 = [Imean_0[2], Imean_2[2], Imean_4[2]]
-Imeans_allU_3 = [Imean_0[3], Imean_2[3], Imean_4[3]]
 
-Imeans_allU_old_0 = [Imean_0_old[0], Imean_2_old[0], Imean_4_old[0]]
-Imeans_allU_old_1 = [Imean_0_old[1], Imean_2_old[1], Imean_4_old[1]]
-Imeans_allU_old_2 = [Imean_0_old[2], Imean_2_old[2], Imean_4_old[2]]
-Imeans_allU_old_3 = [Imean_0_old[3], Imean_2_old[3], Imean_4_old[3]]
+Imeans_allU_old_0 = [Imean_0_old[0], Imean_2_old[0], Imean_4_old[0], Imean_6_old[0]]
+Imeans_allU_old_1 = [Imean_0_old[1], Imean_2_old[1], Imean_4_old[1], Imean_6_old[1]]
+Imeans_allU_old_2 = [Imean_0_old[2], Imean_2_old[2], Imean_4_old[2], Imean_6_old[2]]
+Imeans_allU_old_3 = [Imean_0_old[3], Imean_2_old[3], Imean_4_old[3], Imean_6_old[3]]
+Imeans_allU_old_4 = [Imean_0_old[4], Imean_2_old[4], Imean_4_old[4], Imean_6_old[4]]
+Imeans_allU_old_5 = [Imean_0_old[5], Imean_2_old[5], Imean_4_old[5], Imean_6_old[5]]
+
+
+# Imeans_allU_0 = [Imean_0[0], Imean_2[0], Imean_4[0]]
+# Imeans_allU_1 = [Imean_0[1], Imean_2[1], Imean_4[1]]
+# Imeans_allU_2 = [Imean_0[2], Imean_2[2], Imean_4[2]]
+# Imeans_allU_3 = [Imean_0[3], Imean_2[3], Imean_4[3]]
+# Imeans_allU_4 = [Imean_0[4], Imean_2[4], Imean_4[4]]
+
+# Imeans_allU_old_0 = [Imean_0_old[0], Imean_2_old[0], Imean_4_old[0]]
+# Imeans_allU_old_1 = [Imean_0_old[1], Imean_2_old[1], Imean_4_old[1]]
+# Imeans_allU_old_2 = [Imean_0_old[2], Imean_2_old[2], Imean_4_old[2]]
+# Imeans_allU_old_3 = [Imean_0_old[3], Imean_2_old[3], Imean_4_old[3]]
+# Imeans_allU_old_4 = [Imean_0_old[4], Imean_2_old[4], Imean_4_old[4]]
 
 
 fontsize = 16
@@ -167,13 +191,13 @@ labelsize = 14
 linewidth = 1.
 markersize = 6
 
-fig, ax = plt.subplots(2, 2, figsize=(8, 6))
+fig, ax = plt.subplots(3, 2, figsize=(6, 8), sharex=True)
 
 
 ed_ts = [ts[0], ts[-1]]
 ts_old = [ts[0]]
 
-Us = [0,2,4]
+Us = [0,2,4,6]
 # U = 0
 
 ed_I = asarray([ed_Imean_old[0], ed_Imean[0]])
@@ -194,8 +218,8 @@ for i, U in enumerate(Us):
 # ax.text(2.,0.54,r'$U/\Gamma = 8$',rotation=15, fontsize=10)
 
 
-ax[0,0].set_ylabel(r'$2\pi \mathcal{J}/V$', fontsize=fontsize)
-ax[0,0].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
+ax[0,0].set_ylabel(r'$2\pi \mathcal{J}$', fontsize=fontsize)
+# ax[0,0].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
 ax[0,0].tick_params(axis='both', which='major', labelsize=labelsize)
 ax[0,0].tick_params(axis='y', which='both')
 ax[0,0].locator_params(axis='both', nbins=6)
@@ -203,7 +227,7 @@ ax[0,0].locator_params(axis='both', nbins=6)
 # ax.set_xlim(0, 3)
 ax[0,0].set_title(r'$V/\Gamma=%s$'%(Vs[0]), fontsize=fontsize)
 ax[0,0].legend(fontsize=10)
-
+ax[0,0].annotate(r'(a)', xy=(-0.2, 1.05),xycoords='axes fraction', fontsize=fontsize)
 
 
 ed_I = asarray([ed_Imean_old[1], ed_Imean[1]])
@@ -215,12 +239,13 @@ for i, U in enumerate(Us):
 	ax[0,1].plot(ts, Imeans_allU_1[i] , linewidth=1.5, color='g', marker=markers[i], markersize=markersize, markerfacecolor='none', ls = '--', label=r'$U/\Gamma=%s$'%(U))
 
 
-ax[0,1].set_ylabel(r'$2\pi \mathcal{J}/V$', fontsize=fontsize)
-ax[0,1].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
+ax[0,1].set_ylabel(r'$2\pi \mathcal{J}$', fontsize=fontsize)
+# ax[0,1].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
 ax[0,1].tick_params(axis='both', which='major', labelsize=labelsize)
 ax[0,1].tick_params(axis='y', which='both')
 ax[0,1].locator_params(axis='both', nbins=6)
 ax[0,1].set_title(r'$V/\Gamma=%s$'%(Vs[1]), fontsize=fontsize)
+ax[0,1].annotate(r'(b)', xy=(-0.2, 1.05),xycoords='axes fraction', fontsize=fontsize)
 
 ed_I = asarray([ed_Imean_old[2], ed_Imean[2]])
 # print(ed_I)
@@ -231,12 +256,14 @@ for i, U in enumerate(Us):
 	ax[1,0].plot(ts, Imeans_allU_2[i] , linewidth=1.5, color='g', marker=markers[i], markersize=markersize, markerfacecolor='none', ls = '--', label=r'$U/\Gamma=%s$'%(U))
 
 
-ax[1,0].set_ylabel(r'$2\pi \mathcal{J}/V$', fontsize=fontsize)
-ax[1,0].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
+ax[1,0].set_ylabel(r'$2\pi \mathcal{J}$', fontsize=fontsize)
+# ax[1,0].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
 ax[1,0].tick_params(axis='both', which='major', labelsize=labelsize)
 ax[1,0].tick_params(axis='y', which='both')
 ax[1,0].locator_params(axis='both', nbins=6)
 ax[1,0].set_title(r'$V/\Gamma=%s$'%(Vs[2]), fontsize=fontsize)
+ax[1,0].annotate(r'(c)', xy=(-0.2, 1.05),xycoords='axes fraction', fontsize=fontsize)
+
 
 ed_I = asarray([ed_Imean_old[3], ed_Imean[3]])
 # print(ed_I)
@@ -247,15 +274,54 @@ for i, U in enumerate(Us):
 	ax[1,1].plot(ts, Imeans_allU_3[i] , linewidth=1.5, color='g', marker=markers[i], markersize=markersize, markerfacecolor='none', ls = '--', label=r'$U/\Gamma=%s$'%(U))
 
 
-ax[1,1].set_ylabel(r'$2\pi \mathcal{J}/V$', fontsize=fontsize)
-ax[1,1].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
+ax[1,1].set_ylabel(r'$2\pi \mathcal{J}$', fontsize=fontsize)
+# ax[1,1].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
 ax[1,1].tick_params(axis='both', which='major', labelsize=labelsize)
 ax[1,1].tick_params(axis='y', which='both')
 ax[1,1].locator_params(axis='both', nbins=6)
 ax[1,1].set_title(r'$V/\Gamma=%s$'%(Vs[3]), fontsize=fontsize)
+ax[1,1].annotate(r'(d)', xy=(-0.2, 1.05),xycoords='axes fraction', fontsize=fontsize)
+
+
+
+ed_I = asarray([ed_Imean_old[4], ed_Imean[4]])
+# print(ed_I)
+ax[2,0].plot(ed_ts, ed_I , linewidth=1.5, color='r', marker=markers[0], markersize=markersize, markerfacecolor='none', ls = 'none')
+for i, U in enumerate(Us):
+
+	ax[2,0].plot(ts_old, [Imeans_allU_old_4[i]] , linewidth=1.5, color='c', marker=markers[i], markersize=markersize, markerfacecolor='none', ls = 'none')
+	ax[2,0].plot(ts, Imeans_allU_4[i] , linewidth=1.5, color='g', marker=markers[i], markersize=markersize, markerfacecolor='none', ls = '--', label=r'$U/\Gamma=%s$'%(U))
+
+
+ax[2,0].set_ylabel(r'$2\pi \mathcal{J}$', fontsize=fontsize)
+ax[2,0].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
+ax[2,0].tick_params(axis='both', which='major', labelsize=labelsize)
+ax[2,0].tick_params(axis='y', which='both')
+ax[2,0].locator_params(axis='both', nbins=6)
+ax[2,0].set_title(r'$V/\Gamma=%s$'%(Vs[4]), fontsize=fontsize)
+ax[2,0].annotate(r'(e)', xy=(-0.2, 1.05),xycoords='axes fraction', fontsize=fontsize)
+
+
+ed_I = asarray([ed_Imean_old[5], ed_Imean[5]])
+# print(ed_I)
+ax[2,1].plot(ed_ts, ed_I , linewidth=1.5, color='r', marker=markers[0], markersize=markersize, markerfacecolor='none', ls = 'none')
+for i, U in enumerate(Us):
+
+	ax[2,1].plot(ts_old, [Imeans_allU_old_5[i]] , linewidth=1.5, color='c', marker=markers[i], markersize=markersize, markerfacecolor='none', ls = 'none')
+	ax[2,1].plot(ts, Imeans_allU_5[i] , linewidth=1.5, color='g', marker=markers[i], markersize=markersize, markerfacecolor='none', ls = '--', label=r'$U/\Gamma=%s$'%(U))
+
+
+ax[2,1].set_ylabel(r'$2\pi \mathcal{J}$', fontsize=fontsize)
+ax[2,1].set_xlabel(r'$\Gamma t$', fontsize=fontsize)
+ax[2,1].tick_params(axis='both', which='major', labelsize=labelsize)
+ax[2,1].tick_params(axis='y', which='both')
+ax[2,1].locator_params(axis='both', nbins=6)
+ax[2,1].set_title(r'$V/\Gamma=%s$'%(Vs[5]), fontsize=fontsize)
+ax[2,1].annotate(r'(f)', xy=(-0.2, 1.05),xycoords='axes fraction', fontsize=fontsize)
+
 
 plt.tight_layout(pad=0.5)
 
-# plt.savefig('fig3.pdf', bbox_inches='tight')
+plt.savefig('oneorbital1.pdf', bbox_inches='tight')
 
 plt.show()
