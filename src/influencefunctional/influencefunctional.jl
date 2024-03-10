d60f77d89503c0d691c81d902ab8642e4cbb1e91
guo chu <guochu604b@gmail.com>
2024-03-10 14:54:10 +0800

    minor changes

--- src/influencefunctional/influencefunctional.jl
+++ src/influencefunctional/influencefunctional.jl
@@ -35,7 +35,7 @@ function Base.getproperty(x::TranslationInvariantIF, s::Symbol)
 end
 
 # temporary solution
-changetrunc(x::DMRG1; trunc=x.trunc) = similar(x, D=trunc.D, tolgauge=trunc.ϵ)
+changetrunc(x::DMRG1; trunc=x.trunc) = similar(x, D=trunc.D, tolgauge=trunc.ϵ, tol=trunc.ϵ/10)
 changetrunc(x::DMRG2; trunc=x.trunc) = similar(x, trunc=trunc)
 changetrunc(x::SVDCompression; trunc=x.trunc) = similar(x, D=trunc.D, tol=trunc.ϵ)
 
