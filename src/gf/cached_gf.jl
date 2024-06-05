f745fb5fc0393e38fca827c2cff460aa4b1ca64c
guo chu <guochu604b@gmail.com>
2024-06-05 20:47:43 +0800

    gc changes

--- src/gf/cached_gf.jl
+++ src/gf/cached_gf.jl
@@ -60,9 +60,11 @@ cached_lesser(lattice::Union{RealGrassmannLattice, MixedGrassmannLattice}, i::In
 function cached_occupation(lattice::Union{RealGrassmannLattice, MixedGrassmannLattice}, i::Int, A::Union{GrassmannMPS, Vector}, B::GrassmannMPS...; kwargs...) 
     return real(cached_Gt(lattice, i, i, A, B...; c1=false, c2=true, b1=:+, b2=:-, kwargs...))
 end
-cached_occupation(lattice::Union{RealGrassmannLattice, MixedGrassmannLattice}, A::Union{GrassmannMPS, Vector}, B::GrassmannMPS...; 
-                    cache::AbstractExpectationCache=environments(lattice, A, B...), kwargs...) = [
+cached_occupation(lattice::RealGrassmannLattice, A::Union{GrassmannMPS, Vector}, B::GrassmannMPS...; cache::AbstractExpectationCache=environments(lattice, A, B...), kwargs...) = [
                     cached_occupation(lattice, i, A, B...; cache=cache, kwargs...) for i in 1:lattice.N]
+cached_occupation(lattice::MixedGrassmannLattice, A::Union{GrassmannMPS, Vector}, B::GrassmannMPS...; 
+                    cache::AbstractExpectationCache=environments(lattice, A, B...), kwargs...) = [
+                    cached_occupation(lattice, i, A, B...; cache=cache, kwargs...) for i in 1:lattice.Nt]
 
 
 function cached_electriccurrent(lattice::RealGrassmannLattice1Order, corr::RealCorrelationFunction, k::Int, A::Union{GrassmannMPS, Vector}, B::GrassmannMPS...; 
