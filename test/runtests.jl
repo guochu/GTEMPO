8fb9395019fb4f9302f9cb74c7463bca128eb4e3
guo chu <guochu604b@gmail.com>
2024-10-21 16:04:09 +0800

    gc changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,28 +28,28 @@ const real_ac_grassmann_orderings = [A1A1B1B1a1a1b1b1(), A1A1a1a1B1B1b1b1(), A2A
 const mixed_grassmann_orderings = [A1A1B1B1_A1A1a1a1B1B1b1b1A2A2a2a2B2B2b2b2(), A1A1B1B1_a1a1A1A1b1b1B1B1a2a2A2A2b2b2B2B2(), A1B1B1A1_A2B2B2A2A1B1B1A1a1b1b1a1a2b2b2a2()]
 const mixed_ac_grassmann_orderings = [A1A1B1B1_A1A1a1a1B1B1b1b1A2A2a2a2B2B2b2b2(), A1A1B1B1_a1a1A1A1b1b1B1B1a2a2A2A2b2b2B2B2()]
 
-# include("grassmanntensor.jl")
+include("grassmanntensor.jl")
 
-# include("tempo/grassmannlattice.jl")
-# include("tempo/grassmannmps.jl")
-# include("tempo/bmps_integrate.jl")
+include("tempo/grassmannlattice.jl")
+include("tempo/grassmannmps.jl")
+include("tempo/bmps_integrate.jl")
 
-# include("tempo/partialif_hybrid.jl")
+include("tempo/partialif_hybrid.jl")
 include("tempo/partialif_retardedinteract.jl")
-# include("tempo/influenceoperator.jl")
-# include("tempo/hybriddynamics.jl")
+include("tempo/influenceoperator.jl")
+include("tempo/hybriddynamics.jl")
 
-# include("tempo/gf.jl")
-# include("tempo/cached_gf.jl")
-# include("tempo/bmps_gf.jl")
+include("tempo/gf.jl")
+include("tempo/cached_gf.jl")
+include("tempo/bmps_gf.jl")
 
-# include("tempo/swapband.jl")
-# include("tempo/models.jl")
-# include("tempo/generalimpurity.jl")
-# include("tempo/observables.jl")
+include("tempo/swapband.jl")
+include("tempo/models.jl")
+include("tempo/generalimpurity.jl")
+include("tempo/observables.jl")
 
-# include("tempo/buildK.jl")
+include("tempo/buildK.jl")
 
 
-# ### tempo for interacting systems
-# include("Interacting/neq_tempo.jl")
+### tempo for interacting systems
+include("Interacting/neq_tempo.jl")
